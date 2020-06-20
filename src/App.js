321652407ba944bfb724fa82b3bdd357ae522290
“Meet008” <meetchothani008@gmail.com>
2020-06-20 21:54:23 +0530

    async api integration

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+//import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Cutipie" }),
-    React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "mine" })
-    //React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
+  //   return React.createElement(
+  //     "div",
+  //     {},
+  //     React.createElement("h1", {}, "Adopt Me!"),
+  //     React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Cutipie" }),
+  //     React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "mine" })
+  //     //React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
+  //   );
+
+  return (
+    <div>
+      <h1 id="something-import">Adopt Me!</h1>
+      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
+      <SearchParams />
+    </div>
   );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
---
+++ src/SearchParams.js
@@ -0,0 +1,93 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
+import useDropdown from "./useDropdown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  //   const [animal, setAnimal] = useState("dog");
+  //   const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "Dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+    setPets(animals || []);
+  }
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={(event) => setLocation(event.target.value)}
+          />
+        </label>
+        {/* <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+            onBlur={(e) => setBreed(e.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map((breedString) => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
+        </label> */}
+
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
